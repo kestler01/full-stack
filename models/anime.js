3f4c5cf186ae6426c6fa508354dec33660c829cd
Abdisheik3 <abdisheikh765@gmail.com>
2022-10-06 17:26:40 -0500

    added timestamps

--- models/anime.js
+++ models/anime.js
@@ -1,15 +1,19 @@
 
 const mongoose = require('./connection')
 
-
+const User = require('./user')
 const { Schema, model } = mongoose
 
 
 const animeSchema = new Schema({
     name: String,
     show: String,
-    doesHeSolo: Boolean
-})
+    doesHeSolo: Boolean,
+    owner:{ 
+        type: Schema.Types.ObjectId,
+        ref: 'User'}
+
+    }, { timestamps: true })
 
 
 const Anime = model("Anime", animeSchema)
